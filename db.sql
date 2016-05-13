fb3dd1243abcdc673008af61429ba440ad4f5cbc
Ivan Pushkin <pushkin13@bk.ru>
2016-05-14 01:00:53 +0300

    rewrited db

--- db.sql
+++ db.sql
@@ -1,43 +1,48 @@
 
 CREATE TABLE IF NOT EXISTS `notes` (
-    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-    `title` VARCHAR(300) NOT NULL,
-    `datetime` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    `text` TEXT
+    `id`            INTEGER PRIMARY KEY AUTOINCREMENT,
+    `document_guid` VARCHAR(36) NOT NULL,
+    `title`         VARCHAR(300) NOT NULL,
+    `unixtimestamp` INT8 DEFAULT (strftime('%s', 'now')) NOT NULL,
+    `text`          TEXT
 );
 
 CREATE TABLE IF NOT EXISTS `inflow` (
-    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-    `datetime` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    `name` VARCHAR(300) NOT NULL,
-    `amount` FLOAT NOT NULL,
-    `currency` VARCHAR(3) NOT NULL,
-    `description` TEXT,
-    `source` VARCHAR(300) NOT NULL
+    `id`            INTEGER PRIMARY KEY AUTOINCREMENT,
+    `document_guid` VARCHAR(36) NOT NULL,
+    `unixtimestamp` INT8 DEFAULT (strftime('%s', 'now')) NOT NULL,
+    `name`          VARCHAR(300) NOT NULL,
+    `amount`        FLOAT NOT NULL,
+    `currency`      VARCHAR(3) NOT NULL,
+    `description`   TEXT,
+    `source`        VARCHAR(300) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `outflow` (
-    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-    `datetime` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    `name` VARCHAR(300) NOT NULL,
-    `amount` FLOAT NOT NULL,
-    `currency` VARCHAR(3) NOT NULL,
-    `description` TEXT,
-    `destination` VARCHAR(300) NOT NULL,
-    `target` VARCHAR(300),
-    `count` FLOAT,
-    `metric_unit` VARCHAR(100),
-    `satisfaction` FLOAT
+    `id`            INTEGER PRIMARY KEY AUTOINCREMENT,
+    `document_guid` VARCHAR(36) NOT NULL,
+    `unixtimestamp` INT8 DEFAULT (strftime('%s', 'now')) NOT NULL,
+    `name`          VARCHAR(300) NOT NULL,
+    `amount`        FLOAT NOT NULL,
+    `currency`      VARCHAR(3) NOT NULL,
+    `description`   TEXT,
+    `destination`   VARCHAR(300) NOT NULL,
+    `target`        VARCHAR(300),
+    `count`         FLOAT,
+    `metric_unit`   VARCHAR(100),
+    `satisfaction`  FLOAT
 );
 
 CREATE VIEW IF NOT EXISTS `transaction-list` AS
-    SELECT `datetime`, `name`, `amount`, `currency` FROM
-        (SELECT `datetime`, `name`, `amount`, `currency` FROM `inflow`
-            UNION SELECT `datetime`, `name`, -`amount` AS `amount`, `currency` FROM `outflow`)
-        ORDER BY datetime(`datetime`) DESC;
+    SELECT * FROM (
+        SELECT `document_guid`, `unixtimestamp`, `name`, `amount`, `currency` FROM `inflow`
+        UNION
+        SELECT `document_guid`, `unixtimestamp`, `name`, -`amount` AS `amount`, `currency` FROM `outflow`
+    ) `result_union`
+    ORDER BY (`result_union`.`unixtimestamp`) DESC;
 
 CREATE VIEW IF NOT EXISTS `transactions` AS
     SELECT `t1`.*, SUM(`t2`.`amount`) AS `balance`
         FROM `transaction-list` AS `t1`, `transaction-list` AS `t2`
-            WHERE `t2`.`datetime` <= `t1`.`datetime`
-        GROUP BY `t1`.`datetime` ORDER BY datetime(`t1`.`datetime`) DESC;
+            WHERE `t2`.`unixtimestamp` <= `t1`.`unixtimestamp`
+        GROUP BY `t1`.`document_guid` ORDER BY `t1`.`unixtimestamp` DESC;
--- testdump.sql
+++ testdump.sql
@@ -1,12 +1,12 @@
 
-INSERT INTO `inflow`(`datetime`, `name`, `amount`, `currency`, `source`) VALUES
-    ("2016-05-7 15:30:46", "name1", 2.0, "RUB", "job1"),
-    ("2016-05-8 15:30:46", "name2", 3.0, "RUB", "job2"),
-    ("2016-05-9 15:30:46", "name3", 4.1, "RUB", "job1"),
-    ("2016-05-10 15:30:46", "name4", 5.3, "RUB", "job2");
+INSERT INTO `inflow`(`unixtimestamp`, `document_guid`, `name`, `amount`, `currency`, `source`) VALUES
+    (5, "deadc457-a6b5-4071-911e-b79f29328262", "name1", 2.0, "RUB", "job1"),
+    (2, "8e6ab86d-c53c-4b39-8c7b-5e672c6c887d", "name2", 3.0, "RUB", "job2"),
+    (6, "098a399c-3e97-4ebb-be5e-a35a7cff0e6a", "name3", 4.1, "RUB", "job1"),
+    (8, "de3228eb-9594-4a8e-b97b-8092a2353ba3", "name4", 5.3, "RUB", "job2");
 
-INSERT INTO `outflow`(`datetime`, `name`, `amount`, `currency`, `destination`) VALUES
-    ("2016-05-6 15:20:46", "name5", 3.4, "RUB", "shop1"),
-    ("2016-05-7 15:20:46", "name6", 4.5, "RUB", "shop1"),
-    ("2016-05-9 15:20:46", "name7", 5.1, "RUB", "shop2"),
-    ("2016-05-10 15:20:46", "name8", 6.8, "RUB", "shop2");
+INSERT INTO `outflow`(`unixtimestamp`, `document_guid`, `name`, `amount`, `currency`, `destination`) VALUES
+    (1, "b3493958-b49e-45eb-9d53-a03ce67c25ec", "name5", 3.4, "RUB", "shop1"),
+    (3, "89fb189b-03e1-40bd-b265-feecb8296005", "name6", 4.5, "RUB", "shop1"),
+    (7, "b91f2dcf-ba1a-4e9a-a251-ff49ecebcdae", "name7", 5.1, "RUB", "shop2"),
+    (4, "b7acba0d-7e2b-462d-a460-15bd82918085", "name8", 6.8, "RUB", "shop2");
