f071e5d1902d2d058e8b0096e4c200c2cfa50e40
Ivan Pushkin <pushkin13@bk.ru>
2016-06-07 14:43:06 +0300

    small fixes

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-const GUID_PATTERN = "^[{(]?[0-9A-F]{8}[-]?([0-9A-F]{4}[-]?){3}[0-9A-F]{12}[)}]?$"
-
-type Document interface {
-}
-
-type Inflow struct {
-}
-
-type Outflow struct {
-}
-
 func main() {
 
 }
--- sqlite3_mappers.go
+++ sqlite3_mappers.go
@@ -23,6 +23,7 @@ func (e ErrSQLiteDB) Error() string {
 	return "sqlite db error: " + string(e)
 }
 
+// InitSQLiteDB tries to load sqlite db from file or creates new db file with tables and views
 func InitSQLiteDB(dbFileName string) (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", dbFileName)
 	if err != nil {
@@ -51,4 +52,13 @@ func InitSQLiteDB(dbFileName string) (*sql.DB, error) {
 	}
 
 	return db, err
+}
+
+type Document interface {
+}
+
+type Inflow struct {
+}
+
+type Outflow struct {
 }
