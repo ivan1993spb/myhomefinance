11b11b11d4e0b09984146c6524b2e26f49e64967
Ivan Pushkin <pushkin13@bk.ru>
2016-06-07 14:29:25 +0300

    created init funcs for sqlite mapper

---
+++ sqlite3_mappers.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"io/ioutil"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const DEFAULT_SQLITE_DB_FILE_NAME = "myhomefinance.db"
+
+var dbFileName string = DEFAULT_SQLITE_DB_FILE_NAME
+
+func init() {
+	flag.StringVar(&dbFileName, "sqlite-db", DEFAULT_SQLITE_DB_FILE_NAME, "sqlite db file name")
+}
+
+type ErrSQLiteDB string
+
+func (e ErrSQLiteDB) Error() string {
+	return "sqlite db error: " + string(e)
+}
+
+func InitSQLiteDB(dbFileName string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dbFileName)
+	if err != nil {
+		return db, ErrSQLiteDB(err.Error())
+	}
+
+	if _, err := os.Stat(dbFileName); os.IsNotExist(err) {
+		// Try to open file with queries for tables and views creation and execute that queries
+		f, err := os.Open("sqlite3_db.sql")
+		if err != nil {
+			return db, ErrSQLiteDB("cannot init db: " + err.Error())
+		}
+		defer f.Close()
+
+		rawQuery, err := ioutil.ReadAll(f)
+		if err != nil {
+			return db, ErrSQLiteDB("cannot read file with queries for tables and views creation: " +
+				err.Error())
+		}
+
+		_, err = db.Exec(string(rawQuery))
+		if err != nil {
+			return db, ErrSQLiteDB("cannot execute queries for tables and views creation: " +
+				err.Error())
+		}
+	}
+
+	return db, err
+}
---
+++ sqlite3_mappers_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	TEST_DB_FILE_NAME        = "test.db"
+	TEST_DUMP_DATA_FILE_NAME = "testdump.sql"
+)
+
+type ErrLoadingSQLQuery string
+
+func (e ErrLoadingSQLQuery) Error() string {
+	return "cannot load sql query from file: " + string(e)
+}
+
+func loadSQLQuery(fileName string) (string, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return "", ErrLoadingSQLQuery("cannot open file: " + err.Error())
+	}
+	defer f.Close()
+
+	rawQuery, err := ioutil.ReadAll(f)
+	if err != nil {
+		return "", ErrSQLiteDB("cannot read file: " + err.Error())
+	}
+
+	return string(rawQuery), nil
+}
+
+var EPSILON float64 = 0.00000001
+
+func floatEquals(a, b float64) bool {
+	if (a-b) < EPSILON && (b-a) < EPSILON {
+		return true
+	}
+	return false
+}
+
+func TestInitSQLiteDB(t *testing.T) {
+	db, err := InitSQLiteDB(TEST_DB_FILE_NAME)
+	require.Nil(t, err, "init db return error")
+	defer func() {
+		db.Close()
+		os.Remove(TEST_DB_FILE_NAME)
+	}()
+
+	testDumpQuery, err := loadSQLQuery(TEST_DUMP_DATA_FILE_NAME)
+	require.Nil(t, err, "load dump data sql query")
+
+	db.Exec(testDumpQuery)
+
+	rows, err := db.Query("SELECT `unixtimestamp`, `amount`, `balance` " +
+		"FROM `history` ORDER BY `unixtimestamp` ASC")
+	require.Nil(t, err, "error on selecting history query")
+	defer rows.Close()
+
+	var (
+		checkBalance      float64 = 0
+		transactionNumber int
+	)
+
+	for rows.Next() {
+		transactionNumber++
+
+		var (
+			amount, balance float64
+			unixtimestamp   int
+		)
+		err := rows.Scan(&unixtimestamp, &amount, &balance)
+		require.Nil(t, err, "error on scanning query result")
+
+		checkBalance += amount
+
+		require.True(t, floatEquals(checkBalance, balance),
+			fmt.Sprintf("on transaction %d: %f != %f", transactionNumber, checkBalance, balance))
+	}
+
+	if err := rows.Err(); err != nil {
+		require.Nil(t, err, "error occurred on selecting or scanning rows")
+	}
+}
