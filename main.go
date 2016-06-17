86d07e5b93188c98ecb73b73efde7cf623d6faa9
Ivan Pushkin <pushkin13@bk.ru>
2016-06-17 21:34:55 +0300

    created handler for notes

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/gorilla/mux"
+	"gopkg.in/tylerb/graceful.v1"
+)
+
+const (
+	URL_PATH_NOTES                  = `/notes`
+	URL_PATH_DATE_FROM_DATE_TO      = `/notes/{date_from:\d{4}-\d{2}-\d{2}}_{date_to:\d{4}-\d{2}-\d{2}}`
+	URL_PATH_DATE_FROM_DATE_TO_GREP = `/notes/{date_from:\d{4}-\d{2}-\d{2}}_{date_to:\d{4}-\d{2}-\d{2}}/grep`
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.Path(URL_PATH_NOTES).Methods(http.MethodGet).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path, 1)
+	})
+	r.Path(URL_PATH_NOTES).Methods(http.MethodPut).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path, 2)
+	})
+	r.Path(URL_PATH_NOTES).Methods(http.MethodPost).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path, 3)
+	})
+	r.Path(URL_PATH_NOTES).Methods(http.MethodDelete).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path, 4)
+	})
+	r.Path(URL_PATH_DATE_FROM_DATE_TO).Methods(http.MethodGet).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path, 5)
+	})
+	r.Path(URL_PATH_DATE_FROM_DATE_TO_GREP).Methods(http.MethodGet).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path, 6)
+	})
+
+	graceful.Run(":8888", time.Second, r)
 }
