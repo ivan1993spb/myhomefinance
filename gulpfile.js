c7950e109661f92248ae41887b89efdb6c507ed6
Ivan Pushkin <pushkin13@bk.ru>
2016-08-04 13:35:39 +0300

    created js files for watching

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ gulp.task('styles', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch(["static/src/*.jsx"], ['scripts']);
+    gulp.watch(["static/src/*.jsx", "static/src/*.js"], ['scripts']);
     gulp.watch(["static/src/styles/*.less", "static/src/style/*.css"], ['styles']);
 });
 
