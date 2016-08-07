9857d11716a48941a542695edce75d750fcbc397
Ivan Pushkin <pushkin13@bk.ru>
2016-08-07 23:55:54 +0300

    small fix

--- static/src/dates.js
+++ static/src/dates.js
@@ -6,10 +6,6 @@ exports.yyyymmdd = function(date) {
         day = '' + date.getDate(),
         year = date.getFullYear();
 
-    if (month.length < 2) {
-        month = '0' + month;
-    }
-
     if (day.length < 2) {
         day = '0' + day;
     }
