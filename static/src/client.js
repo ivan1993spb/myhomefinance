9df1897710533c1433dac3c09eba15e6ba4f57ac
Ivan Pushkin <pushkin13@bk.ru>
2016-08-07 23:54:17 +0300

    created api func for creating notes

--- static/src/client.js
+++ static/src/client.js
@@ -4,6 +4,32 @@ var urlPathAPI = '/api';
 var $ = require("jquery");
 var dates = require('./dates');
 
+exports.createNote = function(time, name, text, callback) {
+    var data = {
+        name: name
+    };
+
+    if (!time) {
+        data.time = new Date();
+    }
+
+    if (text) {
+        data.text = text;
+    }
+
+    $.ajax({
+        mathod:  'POST',
+        url:     urlPathAPI + '/note',
+        data:    data,
+        success: function(data, status, xhr) {
+            console.log(data);
+            if (typeof callback === 'function') {
+                callback();
+            }
+        }
+    });
+};
+
 exports.getNotesByDateRange = function(from, to, callback) {
     $.ajax({
         mathod:  'GET',
