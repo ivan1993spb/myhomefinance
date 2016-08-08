224774a806f3c7b5d3ab9e3c243e2676d7355867
Ivan Pushkin <pushkin13@bk.ru>
2016-08-08 16:01:14 +0300

    created test queries

--- test_notes.sql
+++ test_notes.sql
@@ -24,3 +24,27 @@ INSERT INTO `notes` (`unixtimestamp`, `name`, `text`) VALUES
     (strftime('%s', 'now') - 60 * 60 * 13, "name 22", "asassa sdasad asa s a sasa text text"),
     (strftime('%s', 'now') - 60 * 60 * 12, "name 23", "3232 32 32dasdsa sas aas  text text"),
     (strftime('%s', 'now') - 60 * 60 * 11, "name 24", "ghjfgfg assasasatext text");
+
+INSERT INTO inflow (document_guid, unixtimestamp, name, amount, source) VALUES
+    ("203481d2-8026-4613-ae27-c102fb6685df", (strftime('%s', 'now') - 60 * 60 * 1),  "test inflow 1",  10.20, "src"),
+    ("4033f520-dee6-43a0-94d1-4c24dccf3ea3", (strftime('%s', 'now') - 60 * 60 * 24), "test inflow 3",  5.20,  "src"),
+    ("be8de6ee-c44d-4e0b-95d5-9c6d3f47b7ea", (strftime('%s', 'now') - 60 * 60 * 20), "test inflow 5",  2.20,  "src"),
+    ("55e899aa-d259-4aa4-9485-715bc43cc63d", (strftime('%s', 'now') - 60 * 60 * 15), "test inflow 7",  7.50,  "src"),
+    ("b5755d96-7eb3-4310-a780-a3a137b0ec8c", (strftime('%s', 'now') - 60 * 60 * 10), "test inflow 9",  9.20,  "src"),
+    ("ab56893c-0015-4c3a-bbfc-27d5c22be899", (strftime('%s', 'now') - 60 * 60 * 50), "test inflow 11", 11.20, "src"),
+    ("980e8a00-e543-4668-be4c-cbb7ea01aea0", (strftime('%s', 'now') - 60 * 60 * 40), "test inflow 13", 2.20,  "src"),
+    ("9ec4e2fd-bac5-431e-a3ba-c552a43180de", (strftime('%s', 'now') - 60 * 60 * 30), "test inflow 15", 20.20, "src"),
+    ("c8f81266-ebe3-414a-aeb2-16f35048c994", (strftime('%s', 'now') - 60 * 60 * 40), "test inflow 17", 5.20,  "src"),
+    ("4041b245-c626-4db8-90c8-b761d73a3902", (strftime('%s', 'now') - 60 * 60 * 20), "test inflow 19", 10.20, "src");
+
+INSERT INTO outflow (document_guid, unixtimestamp, name, amount, destination) VALUES
+    ("11e1553f-5e33-4e9f-a69a-4f645e24ec88", (strftime('%s', 'now') - 60 * 60 * 2),  "test outflow 2",  1.00, "dst"),
+    ("d5689382-2ee9-47f7-bd43-6443be5463bd", (strftime('%s', 'now') - 60 * 60 * 5),  "test outflow 4",  1.00, "dst"),
+    ("ec0462ad-e119-460b-92af-d6ba710057c4", (strftime('%s', 'now') - 60 * 60 * 10), "test outflow 6",  0.50, "dst"),
+    ("c21c17d2-b5fe-47cd-a3ec-4f01d9ade765", (strftime('%s', 'now') - 60 * 60 * 18), "test outflow 8",  5.00, "dst"),
+    ("c6e51010-8218-40dc-93bb-c65507228e86", (strftime('%s', 'now') - 60 * 60 * 26), "test outflow 10", 2.00, "dst"),
+    ("14d2e901-4c4b-4594-90ca-df5da8351ffc", (strftime('%s', 'now') - 60 * 60 * 30), "test outflow 12", 1.50, "dst"),
+    ("725f05a5-70b8-4c4e-aadd-032a71fca961", (strftime('%s', 'now') - 60 * 60 * 35), "test outflow 14", 3.00, "dst"),
+    ("cdca1120-fb0a-4086-8d10-22e8418ab65d", (strftime('%s', 'now') - 60 * 60 * 31), "test outflow 16", 5.58, "dst"),
+    ("8ece76aa-1381-4389-b79f-5e992cea5f76", (strftime('%s', 'now') - 60 * 60 * 50), "test outflow 18", 1.10, "dst"),
+    ("a359b83f-a720-44d8-af17-b7234e2c6556", (strftime('%s', 'now') - 60 * 60 * 70), "test outflow 20", 8.10, "dst");
