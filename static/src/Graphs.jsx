4f3a00a78cf8e12b4292935dbf7a9fee6398fa3e
Ivan Pushkin <pushkin13@bk.ru>
2016-08-22 16:53:08 +0300

    created balance graph bar

--- static/src/Graphs.jsx
+++ static/src/Graphs.jsx
@@ -1,18 +1,91 @@
 
 var React = require('react'),
+    dates = require("./dates"),
+    client = require("./client"),
     ChartistGraph = require('react-chartist');
+
+var loadDaysLimit = 70;
 
 var Graphs = React.createClass({
     displayName: "Graphs",
 
-    render: function() {
+    propTypes: {
+        dateStart: React.PropTypes.object,
+        loadDays:  React.PropTypes.number
+    },
 
+    getDefaultProps: function() {
+        var currentDate = dates.addDays(new Date(), 1);
+        return {
+            dateStart: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 0, 0, 0, 0),
+            loadDays:  10
+        };
+    },
+
+    getInitialState: function() {
+        return {
+            dateTo:       this.props.dateStart,
+            loadDays:     this.props.loadDays < loadDaysLimit ? this.props.loadDays : loadDaysLimit,
+            balances:     [],
+            transactions: [],
+            loading:      true
+        };
+    },
+
+    handleLoadMore: function() {
+        var from = dates.addDays(this.state.dateTo, -this.state.loadDays),
+            to   = this.state.dateTo;
+
+        this.setState({
+            loading: true,
+            dateTo:  from
+        });
+
+        client.getHistoryRecordsByDateRange(from, to, function(historyRecords) {
+            var newState = {
+                loading: false
+            };
+
+            var balances = [],
+                transactions = [];
+
+            historyRecords.forEach(function(historyRecord) {
+                balances.push(historyRecord.balance);
+                transactions.push(historyRecord.amount);
+            });
+
+            if (balances.length) {
+                balances.reverse();
+                newState.balances = balances;
+            }
+
+            if (transactions.length) {
+                transactions.reverse();
+                newState.transactions = transactions;
+            }
+
+            this.setState(newState);
+        }.bind(this));
+    },
+
+    componentDidMount: function() {
+        this.handleLoadMore();
+    },
+
+    render: function() {
+        console.log(this.state);
+
+        var labels = new Array(this.state.balances.length);
+
+        labels.fill("", 0, this.state.balances.length);
+        labels[0] = this.state.dateTo.toDateString();
+        labels[labels.length-1] = this.state.dateTo.toDateString();
+        console.log(labels);
         var data = {
-            labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+            labels: labels,
             series: [
-                [10, 2,  4,  8,  15, 3,  1,  4,  6,  2,  21],
-                [2,  3,  10, 11, 2,  10, 11, 13, 10, 22, 2],
-                [13, 24, 15, 5,  3,  15, 12, 15, 4,  17, 5]
+                this.state.balances,
+                this.state.transactions
             ]
         };
 
@@ -22,10 +95,10 @@ var Graphs = React.createClass({
                     return index % 2 === 0 ? value : null;
                 }
             },
-            showArea: true
+            // showArea: true
         };
 
-        var type = 'Line';
+        var type = 'Bar';
 
         var style = {
             height: '500px'
@@ -33,6 +106,8 @@ var Graphs = React.createClass({
 
         return (
             <div>
+                <p>Between <i>{this.state.dateTo.toDateString()}</i> and {this.props.dateStart.toDateString()}</p>
+                <div><button type="button">load more</button></div>
                 <ChartistGraph data={data} options={options} type={type} style={style}/>
             </div>
         );
