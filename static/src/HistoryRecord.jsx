2f964417e14be9be9f8d3f3818f89450fe7ce069
Ivan Pushkin <pushkin13@bk.ru>
2016-08-22 13:52:13 +0300

    formatted history output

--- static/src/HistoryRecord.jsx
+++ static/src/HistoryRecord.jsx
@@ -1,6 +1,13 @@
 
 var React = require('react');
 
+function formatMoney(money) {
+    money = Math.round(money*100) / 100;
+    money = (""+money).indexOf(".") > -1 ? money + "00" : money + ".00";
+    var dec = money.indexOf(".");
+    return dec == money.length-3 || dec == 0 ? money : money.substring(0,dec+3);
+}
+
 var HistoryRecord = React.createClass({
     displayName: "HistoryRecord",
 
@@ -13,12 +20,23 @@ var HistoryRecord = React.createClass({
     },
 
     render: function() {
+        var transactionClass = this.props.amount > 0 ? "inflow" : "outflow",
+            transactionText = "";
+
+        if (this.props.amount > 0) {
+            transactionText += "+";
+        } else if (this.props.amount < 0) {
+            transactionText += "-";
+        }
+
+        transactionText += formatMoney(Math.abs(this.props.amount));
+
         return (
             <div className="history-record">
                 <h2>{this.props.name.trim()} {this.props.guid} - {this.props.time.toDateString()} {this.props.time.toTimeString()}</h2>
                 <p>
                     <span className="balance">Balance: {this.props.balance}</span>
-                    <span className={this.props.amount > 0 ? "inflow" : "outflow"}>{this.props.amount}</span>
+                    <span className={transactionClass}>{transactionText}</span>
                 </p>
             </div>
         );
