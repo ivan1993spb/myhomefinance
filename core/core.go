5e67c650584079ad55042a860696e0aba75584cb
Ivan Pushkin <iv.pushk@gmail.com>
2017-04-13 15:49:04 +0300

    creating methods for transactions to core

--- core/core.go
+++ core/core.go
@@ -1,3 +1,24 @@
 package core
 
-type Core struct{}
+import (
+	"time"
+
+	"github.com/ivan1993spb/myhomefinance/models"
+	"github.com/ivan1993spb/myhomefinance/repository"
+)
+
+type Core struct {
+	transactionsRepository repository.TransactionsRepository
+}
+
+func New(transactionsRepository repository.TransactionsRepository) *Core {
+	return &Core{transactionsRepository: transactionsRepository}
+}
+
+func (c *Core) CreateTransaction(transaction *models.Transaction) error {
+	return c.transactionsRepository.CreateTransaction(transaction)
+}
+
+func (c *Core) GetTransactionsByTimeRange(from time.Time, to time.Time) ([]*models.Transaction, error) {
+	return c.transactionsRepository.GetTransactionsByTimeRange(from, to)
+}
