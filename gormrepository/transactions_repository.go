b2758b0f3ef3a1506830377771babf2c44716672
Ivan Pushkin <iv.pushk@gmail.com>
2017-04-13 03:40:32 +0300

    implementing creating transactions in gorm repo

--- gormrepository/transactions_repository.go
+++ gormrepository/transactions_repository.go
@@ -15,9 +15,11 @@ type TransactionsRepository struct {
 
 func NewTransactionsRepository(db *gorm.DB) (*TransactionsRepository, error) {
 	r := &TransactionsRepository{db: db}
+
 	if err := r.init(); err != nil {
 		return nil, fmt.Errorf("cannot create transaction repository: %s", err)
 	}
+
 	return r, nil
 }
 
@@ -25,6 +27,7 @@ func (r *TransactionsRepository) init() error {
 	if err := r.db.AutoMigrate(&transaction{}).Error; err != nil {
 		return fmt.Errorf("cannot initialize table: %s", err)
 	}
+
 	return nil
 }
 
@@ -32,6 +35,19 @@ func (r *TransactionsRepository) GetTransactionsByTimeRange(from time.Time, to t
 	return []*models.Transaction{}, nil
 }
 
-func (r *TransactionsRepository) CreateTransaction(transaction *models.Transaction) error {
+func (r *TransactionsRepository) CreateTransaction(t *models.Transaction) error {
+	newTransaction := &transaction{
+		Time:     t.Time,
+		Amount:   t.Amount,
+		Title:    t.Title,
+		Category: t.Category,
+	}
+
+	if err := r.db.Create(newTransaction).Error; err != nil {
+		return fmt.Errorf("cannot create transaction: %s", err)
+	}
+
+	t.ID = t.ID
+
 	return nil
 }
