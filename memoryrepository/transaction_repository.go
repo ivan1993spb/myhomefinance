7e7115555fd3070e5fcf2b38a672b95521bab238
Ivan Pushkin <iv.pushk@gmail.com>
2017-04-13 12:00:29 +0300

    creating memory transaction repository

---
+++ memoryrepository/transaction_repository.go
@@ -0,0 +1,27 @@
+package gormrepository
+
+import (
+	"time"
+
+	"github.com/ivan1993spb/myhomefinance/models"
+)
+
+type TransactionsRepository struct {
+	transactions []*models.Transaction
+}
+
+func NewTransactionsRepository() (*TransactionsRepository, error) {
+	return &TransactionsRepository{}, nil
+}
+
+func (r *TransactionsRepository) init() error {
+	return nil
+}
+
+func (r *TransactionsRepository) GetTransactionsByTimeRange(from time.Time, to time.Time) ([]*models.Transaction, error) {
+	return []*models.Transaction{}, nil
+}
+
+func (r *TransactionsRepository) CreateTransaction(t *models.Transaction) error {
+	return nil
+}
