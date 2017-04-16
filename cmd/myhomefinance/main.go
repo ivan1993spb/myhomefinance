58f12e2a0414ca877452746b543d04af9db9e17d
Ivan Pushkin <pushkin13@bk.ru>
2017-04-16 04:59:24 +0300

    created cli tool

---
+++ cmd/myhomefinance/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+
+	"github.com/ivan1993spb/myhomefinance/core"
+	"github.com/ivan1993spb/myhomefinance/imports"
+	"github.com/ivan1993spb/myhomefinance/memoryrepository"
+)
+
+func main() {
+	parser := imports.SimpleCSVParser{
+		Reader: csv.NewReader(os.Stdin),
+	}
+	parser.Reader.FieldsPerRecord = 4
+	parser.Reader.Comma = ';'
+	parser.Reader.LazyQuotes = true
+	parser.AddIDs = true
+
+	repository, _ := memoryrepository.NewTransactionsRepository()
+	c := core.New(repository)
+
+	for {
+		t, _ := parser.ReadTransaction()
+		if t == nil {
+			break
+		}
+		c.CreateTransaction(t)
+	}
+
+	inflow, outflow, balance := c.GetStats()
+	fmt.Printf("inflow: %0.2f\noutflow: %0.2f\nbalance: %0.2f\n", inflow, outflow, balance)
+}
