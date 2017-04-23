bf9314a003a35959288209fc79017338ffd87e52
Ivan Pushkin <pushkin13@bk.ru>
2017-04-23 18:43:32 +0300

    implementing handler

--- handlers/create_transaction_handler.go
+++ handlers/create_transaction_handler.go
@@ -1,7 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
 
 	"github.com/ivan1993spb/myhomefinance/core"
 )
@@ -11,4 +16,32 @@ type CreateTransactionHandler struct {
 }
 
 func (h *CreateTransactionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	accountID, err := strconv.ParseUint(vars["account_id"], 10, 64)
+	if err == nil {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	amount, err := strconv.ParseFloat("", 64)
+	if err == nil {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostFormValue("")
+	category := r.PostFormValue("")
+
+	transaction, err := h.core.CreateTransaction(accountID, time.Now(), amount, title, category)
+	if err == nil {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	err := json.NewEncoder(w).Encode(transaction)
+	if err == nil {
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
 }
