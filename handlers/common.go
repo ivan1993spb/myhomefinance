e52633535dd3deb761cbbc663e84de536ac2a157
Ivan Pushkin <iv.pushk@gmail.com>
2017-04-24 16:04:58 +0300

    creating common consts for handlers

---
+++ handlers/common.go
@@ -0,0 +1,17 @@
+package handlers
+
+const (
+	routeVarAccountID     = "account_id"
+	routeVarTransactionID = "transaction_id"
+)
+
+const (
+	fieldAmount   = "amount"
+	fieldTitle    = "title"
+	fieldCategory = "category"
+	fieldTime     = "time"
+)
+
+const (
+	apiDateFormat = "2006-Jan-02"
+)
