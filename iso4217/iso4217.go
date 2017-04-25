93d5fc8d724e90699cf1f7115c24f56cc02aaca6
Ivan Pushkin <iv.pushk@gmail.com>
2017-04-25 11:03:47 +0300

    creating methods for currencies

--- iso4217/iso4217.go
+++ iso4217/iso4217.go
@@ -1,12 +1,39 @@
 package iso4217
 
-import "errors"
+import (
+	"database/sql/driver"
+	"errors"
+)
 
 type Currency byte
 
 func (c Currency) String() string {
 	if code, ok := codes[c]; ok {
 		return code
+	}
+	return defaultCode
+}
+
+func (c Currency) Value() (driver.Value, error) {
+	return c.GetCode(), nil
+}
+
+func (c *Currency) Scan(src interface{}) error {
+	var err error
+	*c, err = Parse(string(src))
+	return err
+}
+
+func (c Currency) GetName() string {
+	if name, ok := names[c]; ok {
+		return name
+	}
+	return defaultName
+}
+
+func (c Currency) GetCode() string {
+	if name, ok := codes[c]; ok {
+		return name
 	}
 	return defaultCode
 }
@@ -510,20 +537,6 @@ var names = map[Currency]string{
 	ZAR: "South Africa Rand",
 	ZMW: "Zambia Kwacha",
 	ZWD: "Zimbabwe Dollar",
-}
-
-func GetName(currency Currency) string {
-	if name, ok := names[currency]; ok {
-		return name
-	}
-	return defaultName
-}
-
-func GetCode(currency Currency) string {
-	if name, ok := codes[currency]; ok {
-		return name
-	}
-	return defaultCode
 }
 
 func Parse(code string) (Currency, error) {
